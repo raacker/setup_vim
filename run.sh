3bf4eb1334a15ac1609b65063159933d94aeadaa
raacker <haven.cpp@gmail.com>
2016-02-20 13:34:05 +0900

    remove install git and fix bugs

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
-apt-get install git
 apt-get install g++
 apt-get install vim
 apt-get install ctags
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim.git
-
 mv ./.vim ./.vimrc ./.themes ~/
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim.git
 
 vim +PluginInstall +qall
