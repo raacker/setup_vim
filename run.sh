b49f9897db775cb959a33eab72eb6428b6428c05
raacker <haven.cpp@gmail.com>
2016-07-06 18:08:56 +0900

    Added Parameterized installation between ubuntu and mint

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-echo " " >> /etc/bash.bashrc
-echo "alias g11='g++ -std=c++11 -o'" >> /etc/bash.bashrc
+if [ $1 == "mint" ]; then
+	echo " " >> /etc/bash.bashrc
+	echo "alias g11='g++ -std=c++11 -o'" >> /etc/bash.bashrc	
+elif [ $1 == "ubuntu" ]; then
+	echo "ubuntu"
+else
+	echo "none of linux version"
+fi
 
 apt-get -y install vim
 apt-get -y install scons 
-apt-get -y install exuberant-ctags
 apt-get -y install g++
+apt-get -y install exuberant-ctags
 
-mv ./.vim ./.vimrc ./.themes ~/
+cp ./.vim ./.vimrc ~/
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
