0123ad87dd4611dde4f1ef678766147ba8dc484b
raacker <haven.cpp@gmail.com>
2016-07-06 21:38:45 +0900

    Added pip installation for python dev

--- run.sh
+++ run.sh
@@ -10,6 +10,16 @@ else
 	echo "none of linux version"
 fi
 
+if [ $2 == "python" ]; then
+	apt-get install python-dev
+	apt-get install python-pip
+	pip install --upgrade pip
+	if [ -f /usr/bin/pip ]; then
+		rm -rf /usr/bin/pip
+	fi
+	sudo ln -s /usr/local/bin/pip /usr/bin/pip
+fi
+
 apt-get -y install vim
 apt-get -y install scons 
 apt-get -y install g++
